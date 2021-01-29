61f992247efebaff3f495f9403367c0abe8633fe
gzamora <gzamora@cartfulsolutions.com>
2021-01-29 11:34:56 -0600

    Improve cors

--- src/server.ts
+++ src/server.ts
@@ -35,15 +35,7 @@ import { V0_USER_MODELS } from './controllers/v0/model.index';
 
   app.use(bodyParser.json());
 
-  app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization',
-    ],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: config.url,
-  }));
+  app.use(cors());
 
   app.use('/api/v0/', IndexRouter);
 
